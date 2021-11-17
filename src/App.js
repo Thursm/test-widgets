5a0e14c7c2c30d4e85a41b923dc2c55d03bb00f1
Thursm <79476877+Thursm@users.noreply.github.com>
2021-11-16 22:05:51 -0700

    First Deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+import Route from "./components/Rout";
+import Header from "./components/Header";
 
-function App() {
+const items = [
+  {
+    title: "What is React",
+    content: "React is a frontend Javascript Framework",
+  },
+  {
+    title: "Why use React",
+    content: "React is a favorite JS Framework",
+  },
+  {
+    title: "How do you use react",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A shade of Blue",
+    value: "blue",
+  },
+];
+
+const showAccordian = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+
+      {/* {showAccordian()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()} */}
+      {/* <Accordion items={items} /> */}
+      {/* <Search /> */}
+      {/* <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button> */}
+      {/* {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null} */}
+      {/* <Translate /> */}
     </div>
   );
-}
-
-export default App;
+};
